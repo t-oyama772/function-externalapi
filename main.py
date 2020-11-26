8d7f30d453c8c9c446497e94e7162297f04f6725
t-oyama772 <>
2020-11-26 22:48:14 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+def external_api_req(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        URL = 'https://api.icndb.com/jokes/random/'
+        r = requests.get(URL)
+        return r.text
